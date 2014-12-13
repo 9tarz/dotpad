3bba811bca2523153e9f31ed7bca0cba07282bd4
9tarz <kanatip.jit@gmail.com>
2014-12-14 04:12:40 +0700

    Key sound arrangement song finished

--- core/Score.py
+++ core/Score.py
@@ -13,6 +13,7 @@ class Score(object):
   def note_collide(self, note):
     judment_line = []
     for i in range (1,5):
+
       #judment_line.append(Rect(60*i,  625,   60,   143))
       judment_line.append(Rect(60*i,  625,   60,   80))
 
@@ -20,28 +21,6 @@ class Score(object):
     #self.test_just(note)
 
     self.just(note, judment_line)
-
-    #if pygame.key.get_pressed()[K_d]:
-      #print note.get_y()
-
-    #for i in range (0,4):
-      #if judment_line[i].colliderect(note.get_rect()) and not note.is_hit :
-        #if pygame.key.get_pressed()[K_d] and i  == 0:
-          #note.hit()
-          #note.play()
-          #self.score += 100
-        #elif pygame.key.get_pressed()[K_f] and i == 1:
-          #note.hit()
-          #note.play()
-          #self.score += 100
-        #elif pygame.key.get_pressed()[K_j] and i == 2:
-          #note.hit()
-          #note.play()
-          #self.score += 100
-        #elif pygame.key.get_pressed()[K_k] and i == 3:
-          #note.hit()
-          #note.play()
-          #self.score += 100
 
   def update(self, note):
 
